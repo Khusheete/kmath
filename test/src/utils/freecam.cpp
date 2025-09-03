4c66c9db050b67ff74d8352db0e32d55512c0b1a
Souchet Ferdinand <Siandfrance@gmail.com>
2025-09-03 23:26:37 +0200

    Added freecam speedup on left shift

--- test/src/utils/freecam.cpp
+++ test/src/utils/freecam.cpp
@@ -1,3 +1,24 @@
+// Copyright © 2025 Souchet Ferdinand (aka. Khusheete)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the “Software”), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+
 #include "freecam.hpp"
 #include "thirdparty/raylib/raylib.h"
 
@@ -27,6 +48,10 @@ void FreeCam::update(float p_delta) {
   }
   if (IsKeyDown(KEY_E)) {
     movement_dir += up;
+  }
+
+  if (IsKeyDown(KEY_LEFT_SHIFT)) {
+    movement_dir *= 2.0f;
   }
 
   position += 2.0f * movement_dir * p_delta;

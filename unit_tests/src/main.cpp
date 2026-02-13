f6d4cd59b1f430e1fdc44832cadb643d42063705
Ferdinand Souchet (Khusheete) <Siandfrance@gmail.com>
2026-02-13 20:47:45 +0100

    Removed print in unit test

--- unit_tests/src/main.cpp
+++ unit_tests/src/main.cpp
@@ -617,7 +617,6 @@ int main(void) {
     UNIT_TEST("sqrt", {
       const Rotor3 a = Rotor3::from_axis_angle(normalized(Vec3(1.0, 2.0, -0.2)), PI * 0.1f);
       const Rotor3 sqrt_a = sqrt(a);
-      std::cout << sqrt_a * sqrt_a << std::endl;
       TEST_EQ_APPROX("sqrt(a) * sqrt(a)", sqrt_a * sqrt_a, a);
     });
   });
